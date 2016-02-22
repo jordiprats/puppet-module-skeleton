2994557cdf5bff8cfe92523a0ca12ac59d39a8d3
Jordi Prats <jprats@systemadmin.es>
2016-02-22 12:35:18 +0100

    multiples usuaris

--- install.sh
+++ install.sh
@@ -2,16 +2,19 @@
 
 #TODO: the skeleton is working only for non-root users.
 
-case $(puppet --version) in
-  3*)
-    #~/.puppet/var/puppet-module/skeleton
-    INSTALLDIR="/usr/lib/ruby/vendor_ruby/puppet/module_tool/skeleton/templates/generator"
-    ;;
-  4*)
-    INSTALLDIR="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/"
-    ;;
-esac
-
+if [ "$(id -u)" -eq 0 ];
+then
+  INSTALLDIR="/usr/lib/ruby/vendor_ruby/puppet/module_tool/skeleton/templates/generator"
+else
+  case $(puppet --version) in
+    3*)
+      INSTALLDIR="$HOME/.puppet/var/puppet-module/skeleton"
+      ;;
+    4*)
+      INSTALLDIR="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/"
+      ;;
+  esac
+fi
 rm -fr ${INSTALLDIR}
 cp -pr skeleton ${INSTALLDIR}
 find ${INSTALLDIR} -iname .gitkeep -empty -delete
